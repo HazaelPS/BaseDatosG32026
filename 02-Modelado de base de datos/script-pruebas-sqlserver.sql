7abb91b347cef5431c959f154ac89e735d9e2ee6
HazaelPS <saddayhazaelpinedasoledad@gmail.com>
2026-07-19 19:33:10 -0600

    Se terminaron los temas de SQL-SDD con los comandos CREATE, ALTER, DROP, aplicados a tablas y restricciones. Se comenzó con el trabajo de creación de un diagrama lógico a físico aplicando cada una de las instrucciones SQL.

--- 02-Modelado de base de datos/script-pruebas-sqlserver.sql
+++ 02-Modelado de base de datos/script-pruebas-sqlserver.sql
@@ -1,49 +1,54 @@
-# Crea una base de datos
+-- Crea una base de datos
 CREATE DATABASE prueba_atributos;
+GO
 
-# Utiliza la base de datos
+-- Utiliza la base de datos
 USE prueba_atributos;
+GO
 
-# Crea una tabla
+-- Crea una tabla
 CREATE TABLE empleado(
 	id INT NOT NULL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido_1 VARCHAR(15),
-    apellido_2 VARCHAR(15) NULL,
-    fecha_ingreso DATE NOT NULL
+	nombre VARCHAR(50) NOT NULL,
+	apellido_1 VARCHAR(15),
+	apellido_2 VARCHAR(15) NULL,
+	fecha_ingreso DATE NOT NULL
 );
 
-# Inserta un valor
-INSERT INTO empleado VALUES (1, "Reberiano", "Robles", "De la Torre", '1983-04-06'); 
+-- Inserta un valor
+INSERT INTO empleado VALUES (1, 'Reberiano', 'Robles', 'De la Torre', '1983-04-06');
 
-# Agrega un nuevo campo
-ALTER TABLE empleado ADD COLUMN
+-- Agrega un nuevo campo
+ALTER TABLE empleado ADD
 fecha_nacimiento DATE NOT NULL;
 
-# Inserta un nuevo valor
-INSERT INTO empleado VALUES (2, "Rómulo", "Robles", "Esparza", '1984-07-12', '1976-06-07');
+-- Inserta un nuevo valor
+INSERT INTO empleado VALUES (2, 'Rómulo', 'Robles', 'Esparza', '1984-07-12', '1976-06-07');
 
-# Muestra los datos en la tabla y separa la feha en año, mes y día
+-- Muestra los datos en la tabla y separa la fecha en año, mes y día
 SELECT *, YEAR(fecha_ingreso) AS anio, MONTH(fecha_ingreso) AS mes, DAY(fecha_ingreso) AS dia
 FROM empleado;
 
-# Muestra todos los registros
+-- Muestra todos los registros
 SELECT * FROM empleado;
 
-# Modifica la fecha de nacimiento
+-- Modifica la fecha de nacimiento
 UPDATE empleado SET fecha_nacimiento = '1983-04-06' WHERE id = 1;
 
-# Muestra los datos y la edad
+-- Muestra los datos y la edad
 SELECT
 	nombre,
-    apellido_1,
-    apellido_2, 
-    fecha_ingreso,
-    fecha_nacimiento,
-    TIMESTAMPDIFF(year, fecha_nacimiento, CURDATE()) AS edad
+	apellido_1,
+	apellido_2,
+	fecha_ingreso,
+	fecha_nacimiento,
+	DATEDIFF(YEAR, fecha_nacimiento, GETDATE()) AS edad
 FROM empleado;
 
 
+-- Crea la segunda base de datos antes de usarla
+CREATE DATABASE ejemplobd;
+GO
 
 USE ejemplobd;
 GO
@@ -53,7 +58,7 @@ CREATE TABLE alumno(
 	nombre VARCHAR(30) NOT NULL,
 	apellido_1 VARCHAR(15) NOT NULL,
 	apellido_2 VARCHAR(15) NULL,
-	telefono VARCHAR(13), 
+	telefono VARCHAR(13),
 	calle VARCHAR(50) NOT NULL,
 	numero_interior INT NULL,
 	numero_exterior INT NULL,
@@ -71,7 +76,6 @@ VALUES (3, 'Jimena', 'Valdez', 'Delgadillo', '5 de mayo', '2007-06-12');
 
 SELECT *, DATEDIFF(YEAR, fecha_nacimiento, GETDATE()) AS edad
 FROM alumno;
-
 
 
 CREATE TABLE categoria2(
